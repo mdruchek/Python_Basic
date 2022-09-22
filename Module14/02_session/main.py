44ec652f941605ac5598bdf4e56e8033672b1818
Михаил Дручек <druchek_ma@vsw.ru>
2022-09-22 15:21:40 +0000

    Update main.py

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,17 +1,1 @@
-print("Введите первую точку")
-x1 = float(input('X: '))
-y1 = float(input('Y: '))
-print("\nВведите вторую точку")
-x2 = float(input('X: '))
-y2 = float(input('Y: '))
-
-x_diff = x1 - x2
-y_diff = y1 - y2
-if x_diff == 0:
-    print('Уравнение вида y = k * x + b составить нельзя!')
-else:
-    k = y_diff / x_diff
-    b = y2 - k * x2
-
-    print("Уравнение прямой, проходящей через эти точки:")
-    print('y = ', k, " * x + ", b)
+# TODO здесь писать код
