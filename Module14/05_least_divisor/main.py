efbc31314c2032218af742505e59f38844bd0373
dr <dr@rosa2021.1>
2022-09-22 17:40:08 +0300

    added m14 homework

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def smallest_divisor(number):
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            return divisor
+    return number
+
+
+number = int(input('Введите число: '))
+print('Наименьший делитель, отличный от единицы: ', smallest_divisor(number))
