cb7001bde38e565e953ec8bb18d1a6084241d7bd
Михаил Дручек <druchek_ma@vsw.ru>
2022-09-22 15:24:19 +0000

    Update main.py

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,14 +1,1 @@
-import math
-
-def point_in_circle(x, y):
-    r = math.sqrt(x ** 2 + y ** 2)
-    if r <= 1:
-        print('Монетка где-то рядом')
-    else:
-        print('Монетки в области нет: ')
-
-
-print('Введите координаты монетки: ')
-x = float(input('X: '))
-y = float(input('У: '))
-point_in_circle(x, y)
+# TODO здесь писать код
