8dc36d56778c3b8520e8f1598a64e39bf446f69a
Михаил Дручек <druchek_ma@vsw.ru>
2022-09-22 15:23:54 +0000

    Update main.py

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,9 +1,1 @@
-def smallest_divisor(number):
-    for divisor in range(2, number):
-        if number % divisor == 0:
-            return divisor
-    return number
-
-
-number = int(input('Введите число: '))
-print('Наименьший делитель, отличный от единицы: ', smallest_divisor(number))
+# TODO здесь писать код
