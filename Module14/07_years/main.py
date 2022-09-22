17ca08bd6d157b8391d8833db6896f11353dd2b9
Михаил Дручек <druchek_ma@vsw.ru>
2022-09-22 15:24:41 +0000

    Update main.py

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,16 +1,1 @@
-def identical_numbers(year):
-    for figure_1 in str(year):
-        count = 0
-        for figure_2 in str(year):
-            if figure_1 == figure_2:
-                count += 1
-    if count == 3:
-        print(year)
-
-
-year_1 = int(input('Введите первый год: '))
-year_2 = int(input('Введите второй год: '))
-
-print('Годы от', year_1, ' до ', year_2, ' с тремя одинаковыми цифрами: ')
-for year in range(year_1, year_2 + 1):
-    identical_numbers(year)
+# TODO здесь писать код
