dc81addbe0fb48cb3aad85363835a5615e4347e5
Mikhail Druchek <druchek_ma@vsw.ru>
2022-09-27 22:34:54 +0300

    tasks 9 and 10 have not been completed

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,10 +1,15 @@
 quantity_friends = int(input('Кол-во друзей: '))
 quantity_receipt = int(input('Долговых расписок: '))
-balance = [[], [], []]
+bank = []
+balans = []
 
-for i_receipt in range(quantity_receipt):
-    print('\n', i_receipt, '-я расписка')
+for _ in range(quantity_receipt):
+    print('\n', i_receipt + 1, '-я расписка')
     to_whom = int(input('Кому: '))
     from_whom = int(input('От кого: '))
     money = int(input('Сколько: '))
-    
+    receipt = [to_whom, from_whom, money]
+    bank.append(receipt)
+
+for _ in range(quantity_friends):
+    for __ in _:
--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+quantity_number = int(input('Кол-во чисел: '))
+initial_list = []
+
+for _ in range(quantity_number):
+    initial_list.append(int(input('Число: ')))
+print('Последовательность: ', initial_list)
+
+for _ in range(len(initial_list) // 2 + 1):
+    flag = True
+    for __ in range(len(initial_list) // 2 + 1):
+        if initial_list[-len(initial_list) // 2 - __ - 1] != initial_list[-len(initial_list) // 2 + __]:
+            flag = False
+            break
+    if flag:
