6e982c02be87d66640f9e624f69adcd34e29f408
Mihail Druchek <druchek_ma@vsw.ru>
2022-09-28 11:14:13 +0300

    task 9 is solved

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,15 +1,21 @@
 quantity_friends = int(input('Кол-во друзей: '))
 quantity_receipt = int(input('Долговых расписок: '))
-bank = []
-balans = []
+balance = []
+for _ in range(quantity_friends):
+    balance.append(0)
 
-for _ in range(quantity_receipt):
+for i_receipt in range(quantity_receipt):
     print('\n', i_receipt + 1, '-я расписка')
     to_whom = int(input('Кому: '))
     from_whom = int(input('От кого: '))
     money = int(input('Сколько: '))
-    receipt = [to_whom, from_whom, money]
-    bank.append(receipt)
+    for i_friend in range(quantity_friends):
+        if to_whom == i_friend + 1:
+            balance[i_friend] = balance[i_friend] - money
 
-for _ in range(quantity_friends):
-    for __ in _:
+        if from_whom == i_friend + 1:
+            balance[i_friend] = balance[i_friend] + money
+
+print('\nБаланс друзей:')
+for i_friend in range(len(balance)):
+    print(i_friend + 1, ': ', balance[i_friend])
