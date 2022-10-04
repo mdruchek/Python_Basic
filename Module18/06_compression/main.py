6e4d77bad3ce15039d8433d58a2d1815c2594e23
Mikhail Druchek <druchek_ma@vsw.ru>
2022-10-04 22:00:01 +0300

    task 10 saved

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,6 @@
-_str = input('Введите строку: ')
+unencoded_string = input('Введите строку: ')
+
+unencoded_list_str = [simbol for simbol in unencoded_string]
+encoded_list_str = [unencoded_list_str.remove(index - 1]) if list_str[index] == list_str[index - 1] else list_str.insert(index + 1, 3) for index in range(1, len(list_str))]
+
+print(list_str)
--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,1 +1,53 @@
-# TODO здесь писать код
+def shift_word(shift, word_str):
+    if word_str != '':
+        word_list = [symbol for symbol in word_str]
+        for transfer_symbol in range(shift):
+            word_list.insert(0, word_list[len(word_list) - 1])
+            word_list.pop()
+        word_str_shift = ''
+        for symbol in word_list:
+            word_str_shift += symbol
+        return word_str_shift
+
+
+def special_characters(symbol):
+    if symbol == ' ':
+        return ' '
+    if symbol == '.':
+        return ''
+    if symbol == '/':
+        return ''
+    if symbol == '(':
+        return ''
+    if symbol == '+':
+        return ''
+    if symbol == '"':
+        return ''
+    if symbol == '-':
+        return ''
+
+
+def decoded_text(text):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    shift = 51
+    caesar_decoder_symbol_list = [alphabet[(alphabet.index(symbol) + shift) % len(alphabet)]
+                      if symbol != ' ' and symbol != '.' and symbol != '/' and symbol != '(' and symbol != '+' and symbol != '"' and symbol != '-'
+                      else special_characters(symbol)
+                      for symbol in text]
+
+    caesar_decoder_str = ''
+    for symbol in caesar_decoder_symbol_list:
+        caesar_decoder_str += symbol
+
+    caesar_decoder_word_list = caesar_decoder_str.split(' ')
+    caesar_decoder_word_shift_list = [shift_word(3, word_str) for word_str in caesar_decoder_word_list]
+
+    return caesar_decoder_word_shift_list
+
+
+
+text = 's .. fu(tm pe psfn gp tf"uip'
+
+decoded = decoded_text(text)
+
+print(decoded)
