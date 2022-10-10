35c3de266c76970f5af3cfb4928a88163ae07f7d
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-10 07:48:42 +0300

    recursion in task 9

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,3 +1,12 @@
+def reading_tree(_dict):
+    global height
+    height += 1
+    for key in _dict:
+        tree_set[key] = height
+        reading_tree(_dict[key])
+    return
+
+
 def adding_descendant(_dict, _list):
     if _list[1] in _dict.keys():
         _dict[_list[1]][_list[0]] = {}
@@ -8,16 +17,19 @@ def adding_descendant(_dict, _list):
 
 quantity_people = int(input('Введите количество человек: '))
 
+height = 0
+tree_set = dict()
 count = ['Первая', 'Вторая', 'Третья', 'Четвёртая', 'Пятая', 'Шестая', 'Седьмая', 'Восьмая']
 family_tree = dict()
+
 for index in range(quantity_people - 1):
-    names_set = set()
     couple_list = input('{} пара: '.format(count[index])).split()
-    names_set.update(set(couple_list))
     if index == 0:
         family_tree[couple_list[1]] = {couple_list[0]: {}}
     else:
         adding_descendant(family_tree, couple_list)
 
+reading_tree(family_tree)
+
 print(family_tree)
-print(sorted(names_set))
+print(tree_set)
