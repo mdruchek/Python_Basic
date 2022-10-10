68697e88ec31d8746239498c0cf04213059c3062
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-10 08:49:47 +0300

    module 19 is solved

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,35 +1,15 @@
-def reading_tree(_dict):
-    global height
-    height += 1
-    for key in _dict:
-        tree_set[key] = height
-        reading_tree(_dict[key])
-    return
-
-
-def adding_descendant(_dict, _list):
-    if _list[1] in _dict.keys():
-        _dict[_list[1]][_list[0]] = {}
-        return
-    for key in _dict.keys():
-        adding_descendant(_dict[key], _list)
-
-
 quantity_people = int(input('Введите количество человек: '))
 
-height = 0
-tree_set = dict()
 count = ['Первая', 'Вторая', 'Третья', 'Четвёртая', 'Пятая', 'Шестая', 'Седьмая', 'Восьмая']
 family_tree = dict()
 
 for index in range(quantity_people - 1):
     couple_list = input('{} пара: '.format(count[index])).split()
-    if index == 0:
-        family_tree[couple_list[1]] = {couple_list[0]: {}}
+    if couple_list[1] not in family_tree.keys():
+        family_tree[couple_list[1]] = 0
+        family_tree[couple_list[0]] = 1
     else:
-        adding_descendant(family_tree, couple_list)
+        family_tree[couple_list[0]] = family_tree[couple_list[1]] + 1
 
-reading_tree(family_tree)
-
-print(family_tree)
-print(tree_set)
+print('\nВысота каждого члена семьи:')
+{print('{} {}'.format(key, family_tree[key])) for key in sorted(family_tree.keys())}
--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def checking_palindrome(text):
+    palindrome = True
+    number_of_odd_values = 0
+    symbol_dict = dict()
+
+    for symbol in text:
+        if symbol in symbol_dict.keys():
+            symbol_dict[symbol] += 1
+        else:
+            symbol_dict[symbol] = 1
+
+    for value in symbol_dict.values():
+        if value % 2 == 1:
+            number_of_odd_values += 1
+
+    if len(text) % 2 == 1 and number_of_odd_values == 1\
+            or len(text) % 2 == 0 and number_of_odd_values == 0:
+        print('Можно сделать палиндромом')
+    else:
+        print('Нельзя сделать палиндромом')
+
+
+text_list = input('Введите строку: ')
+
+checking_palindrome(text_list)
