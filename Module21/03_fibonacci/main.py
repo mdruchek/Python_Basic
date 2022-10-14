fb140c458b84a57b8116d06ce028172e9b80404a
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-14 08:28:04 +0300

    1,3,4,8 tasks are solved

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def fibonachi(num_pos_user, num_pos_current=1, elem=None,
+              elem_previous_1=None, elem_previous_2=None):
+    if num_pos_current == 1:
+        elem = 1
+    elif num_pos_current == 2:
+        elem = 1
+        elem_previous_1 = 1
+    else:
+        elem_previous_2 = elem_previous_1
+        elem_previous_1 = elem
+        elem = elem_previous_1 + elem_previous_2
+
+    if num_pos_current != num_pos_user:
+        num_pos_current += 1
+        fibonachi(num_pos_user, num_pos_current, elem, elem_previous_1, elem_previous_2)
+    else:
+        return number.append(elem)
+
+number = []
+num_pos = int(input('Введите позицию числа Фибоначчи: '))
+fibonachi(num_pos)
+print('Число: {}'.format(number[0]))
