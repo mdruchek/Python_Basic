b8109287a0613a665a22659a870487cd11d8e7f7
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-14 07:38:34 +0300

    tasks 1 and 4 are solved

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,40 @@
-# TODO здесь писать код
+site = {
+	'html': {
+		'head': {
+			'title': 'Мой сайт'
+		},
+		'body': {
+			'h2': 'Здесь будет мой заголовок',
+			'div': 'Тут, наверное, какой-то блок',
+			'p': 'А вот здесь новый абзац'
+		}
+	}
+}
+
+def search_key(site, key, depth = None):
+	if key in site:
+		return site[key]
+	for i_value in site.values():
+		if isinstance(i_value, dict):
+			if depth is not None:
+				depth -= 1
+				if depth == 0:
+					result = None
+					break
+			result = search_key(i_value, key, depth)
+			if result:
+				break
+	else:
+		result = None
+
+	return result
+
+key = input('Введите искомый ключ: ')
+question = input('Хотите ввести максимальную глубину? Y/N: ').lower()
+if question == 'y':
+	depth = int(input('Введите максимальную глубину: '))
+	value = search_key(site, key, depth=depth)
+else:
+	value = search_key(site, key)
+
+print('Значение ключа: {}'.format(value))
