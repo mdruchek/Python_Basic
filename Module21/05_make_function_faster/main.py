5a7be0b0407b986794ca9e05380e46326eb215cb
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-14 08:56:09 +0300

    tasks 1,3,4,5,8 is solved

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,11 @@
-def calculating_math_func(data):
+def calculating_math_func(data, faktorial = dict()):
     result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
+    if data not in faktorial:
+        for index in range(1, data + 1):
+            result *= index
+        faktorial[data] = result
+    else:
+        result = faktorial[data]
+    result = faktorial[data] / data ** 3
     result = result ** 10
     return result
-
-# TODO оптимизировать функцию
