23e0cb74d4129134a925109ca5f26875dc52526c
Mikhail Druchek <druchek_ma@vsw.ru>
2022-10-16 20:26:27 +0300

    tasks 1,3,4,5,6,7,8 is solved

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def sum(*args, _sum=[0]):
+    if (isinstance(args, tuple) and len(args) == 1) or isinstance(args[0], list):
+        args = args[0]
+    for elem in args:
+        if isinstance(elem, list):
+            sum(elem, _sum[0])
+        else:
+            _sum[0] += elem
+    return _sum[0]
+
+#print(sum((1, 2, 3, 4, 5)))
--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,13 +1,13 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-def open_list(nice_list):
+def open_list(nice_list, new_list = []):
     for elem in nice_list:
         if isinstance(elem, list):
             open_list(elem)
         else:
             new_list.append(elem)
+    return new_list
 
-new_list = []
-open_list(nice_list)
-print('Ответ: {}'.format(new_list))
+
+#print('Ответ: {}'.format(open_list(nice_list)))
