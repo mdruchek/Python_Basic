32d8916b735a30cc465f9a6748604461920eb0ae
Mikhail Druchek <druchek_ma@vsw.ru>
2022-10-17 22:59:54 +0300

    task 9 is modified_2

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,49 +1,60 @@
-def move(n, x, y, z=0, start=True, rods=dict()):
+def move(n, x, y, z=0, start=True, rods=dict(), source_rod=0):
     if start:
+        source_rod = x
         z = 6 - x - y
         rods = {x: [i for i in range(n, 0, -1)], y: [], z: []}
-    if start or rods[x] == [cound for cound in range(len(rods[x]), 0, -1)]:
-        rods[z].append(rods[x].pop())
-        rods[y].append(rods[x].pop())
-        rods[y].append(rods[z].pop())
-    if not start:
-        if len(rods[y]) == 0 and n != 0:
-            rods[y].append(rods[x].pop())
-            rods[x].append(rods[z].pop())
-            rods[y].append(rods[z].pop())
-            rods[y].append(rods[x].pop())
-        if len(rods[z]) == 0 and n != 0:
-            rods[z].append(rods[x].pop())
-            rods[x].append(rods[y].pop())
-            rods[z].append(rods[y].pop())
-            rods[z].append(rods[x].pop())
-        if len(rods[x]) == 0:
-            rods[x].append(rods[y].pop())
-            rods[y].append(rods[z].pop())
-            rods[x].append(rods[z].pop())
-            rods[x].append(rods[y].pop())
-            rods[z].append(rods[y].pop())
-        if len(rods[x]) != 0 and len(rods[y]) != 0 and len(rods[z]) != 0 and n != 0:
-            if len(rods[y]) > len(rods[z]):
-                rods[z].append(rods[y].pop())
-                rods[x].append(rods[y].pop())
-                rods[x].append(rods[z].pop())
-                rods[y].append(rods[z].pop())
-                rods[z].append(rods[x].pop())
-                rods[y].append(rods[x].pop())
-                rods[y].append(rods[z].pop())
-            if len(rods[z]) > len(rods[y]):
-                rods[y].append(rods[z].pop())
-                rods[x].append(rods[z].pop())
-                rods[x].append(rods[y].pop())
-                rods[y].append(rods[y].pop())
-                rods[y].append(rods[x].pop())
-                rods[z].append(rods[x].pop())
-                rods[z].append(rods[y].pop())
+    if rods[source_rod] == 0:
+        for rod, val in rods.items():
+            if len(val) != 0 and val[len(val) - 1] == 1:
+                source_rod = rod
+    rods[z].append(rods[x].pop())
+    rods[y].append(rods[x].pop())
+    rods[y].append(rods[z].pop())
 
-    if len(rods[y]) != n:
-        move(n, x, y, z, False, rods)
+    flag_disk_odd = False
+    flag_rod_null = False
+    for rod, val in rods.items():
+        if len(val) == 0:
+            rod_null = rod
+            flag_rod_null = True
+        if len(val) != 0 and val[len(val) - 1] == 1:
+            rod_num_disk_12 = rod
+
+    if rod_num_disk_12 != source_rod:
+        if flag_rod_null:
+            rods[rod_null].append(rods[source_rod].pop())
+        else:
+            min_disk_excluding_12 = n
+            for rod, val in rods.items():
+                if rod != rod_num_disk_12 and val[len(val) - 1] < min_disk_excluding_12:
+                    min_disk_excluding_12 = val[len(val) - 1]
+                    rod_min_disk_excluding_12 = rod
+            rods[6 - rod_min_disk_excluding_12 - rod_num_disk_12].append(rods[rod_min_disk_excluding_12].pop())
     else:
-        return
+        min_disk_excluding_12 = n
+        for rod, val in rods.items():
+            if rod != rod_num_disk_12 and val[len(val) - 1] < min_disk_excluding_12:
+                min_disk_excluding_12 = val[len(val) - 1]
+                rod_min_disk_excluding_12 = rod
+        rods[6 - rod_min_disk_excluding_12 - rod_num_disk_12].append(rods[rod_min_disk_excluding_12].pop())
 
-move(5, 1, 2)
+    for rod, val in rods.items():
+        if len(val) != 0 and val[len(val) - 1] % 2 != 0 and val[len(val) - 1] != 1:
+            rod_num_disk_odd = rod
+            flag_disk_odd = True
+
+    for rod, val in rods.items():
+        if len(val) == 0:
+            rod_null = rod
+            flag_rod_null = True
+
+
+
+    if flag_disk_odd:
+        destination_disk = rod_num_disk_odd
+    else:
+        destination_disk = rod_null
+    move(n, rod_num_disk_12, destination_disk, z=6-rod_num_disk_12-destination_disk, start=False, rods=rods, source_rod=source_rod)
+
+
+move(6, 1, 2)
