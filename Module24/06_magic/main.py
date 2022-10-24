ee703dbe2b706124cd8b75c0ceba8b2eab9ab4d1
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-24 15:30:48 +0300

    tasks 1,2,3,4,5,6 module 24 are solved

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,75 @@
-# TODO здесь писать код
+class Water:
+    def __init__(self):
+        print('класс "Вода"', end='')
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(self, Ground):
+            return Mud()
+        else:
+            return None
+
+
+class Air:
+    def __init__(self):
+        print('класс "Воздух"', end='')
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Dust()
+        else:
+            return None
+
+
+class Fire:
+    def __init__(self):
+        print('класс "Огонь"', end='')
+    def __add__(self, other):
+        if isinstance(other, Ground):
+            return Lava()
+        else:
+            return None
+
+
+class Ground:
+    def __init__(self):
+        print('класс "Земля"', end='')
+
+
+class Storm:
+    def __init__(self):
+        print('класс "Шторм"')
+
+class Steam:
+    def __init__(self):
+        print('класс "Пар"')
+
+
+class Mud:
+    def __init__(self):
+        print('класс "Грязь"')
+
+
+class Lightning:
+    def __int__(self):
+        print('класс "Молния"')
+
+class Dust:
+    def __init__(self):
+        print('класс "Пыль"')
+
+class Lava:
+    def __init__(self):
+        print('класс "Лава"')
+
+
+a = Water()
+print(' плюс ', end='')
+b = Fire()
+print(' получился ', end='')
+c = a + b
--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -1,1 +1,101 @@
-# TODO здесь писать код
+import random
+
+
+class Drawer:
+    def __init__(self):
+        self.money = 0
+
+    def give_money_away(self):
+        if self.money > 0:
+            self.money -= 10
+
+    def accept_money(self):
+        self.money += 30
+
+    def return_amount_money(self):
+        print('Количество денег в тумбочке {}.'.format(self.money))
+        return self.money
+
+
+class Fridge:
+    def __init__(self):
+        self.food = 50
+
+    def give_away_food(self):
+        if self.food > 0:
+            self.food -= 10
+
+    def take_food(self):
+        if self.food <= 70:
+            self.food += 30
+
+    def return_amount_food(self):
+        print('Количество еды в холодильнике {}.'.format(self.food))
+        return self.food
+
+
+class House:
+    def __init__(self):
+        self.drawer = Drawer()
+        self.fridge = Fridge()
+        self.dwellers = []
+
+    def settle_guest(self, dweller):
+        print('{} заселился в дом.'.format(dweller.name))
+        self.dwellers.append(dweller)
+
+    def evict_guest(self, dweller):
+        print('{} покинул дом.'.format(dweller.name))
+        self.dwellers.remove(dweller)
+
+
+class Human:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+        self.degree_satiety = 50
+        house.settle_guest(self)
+
+    def eat(self):
+        print('{} ест.'.format(self.name))
+        self.house.fridge.give_away_food()
+
+    def work(self):
+        print('{} работает'.format(self.name))
+        self.house.drawer.accept_money()
+
+    def play(self):
+        print('{} играет.'.format(self.name))
+        self.degree_satiety -= 10
+
+    def go_to_grocery_store_for_food(self):
+        print('{} идёт в магазин за едой.'.format(self.name))
+        self.house.drawer.give_money_away()
+        self.house.fridge.take_food()
+
+    def what_to_do(self):
+        cube = random.randint(1, 6)
+        if self.degree_satiety < 20:
+            self.eat()
+        elif self.house.fridge.return_amount_food() < 10:
+            self.go_to_grocery_store_for_food()
+        elif self.house.drawer.return_amount_money() < 50:
+            self.work()
+        elif cube == 1:
+            self.work()
+        elif cube == 2:
+            self.eat()
+        else:
+            self.play()
+
+    def is_alive(self):
+        if self.degree_satiety > 0:
+            return True
+        return False
+
+
+house = House()
+human1 = Human('Артем', house)
+human2 = Human('Марина', house)
+number_day = 0
+while number_day < 365
