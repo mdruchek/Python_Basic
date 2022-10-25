7771b357aa97d13b0d430fae52a05fb7c0155072
Mihail Druchek <druchek_ma@vsw.ru>
2022-10-25 10:38:38 +0300

    task 8 module 24 is solved

--- Module24/08_blackjack/main.py
+++ Module24/08_blackjack/main.py
@@ -1,1 +1,120 @@
-# TODO здесь писать код
+import random
+
+
+class Card:
+    weights = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+              'Валет': 10, 'Дама': 10, 'Король': 10, 'Туз': 11}
+
+    def __init__(self, suit, weight):
+        self.suit = suit
+        self.weight = weight
+
+    def weight_return(self):
+        return self.weights[self.weight]
+
+    def return_card(self):
+        return self.weight, self.suit
+
+
+class Deck:
+    suits = ['Червы', 'Бубны', 'Пики', 'Крести']
+    weights = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Дама', 'Король', 'Туз']
+
+    def __init__(self):
+        self.deck_cards = [Card(suit, weight) for suit in self.suits for weight in self.weights]
+
+    def hand_over_card(self):
+        number_card = random.randint(0, len(self.deck_cards) - 1)
+        card_to_be_dealt = self.deck_cards[number_card]
+        self.deck_cards.remove(card_to_be_dealt)
+        return card_to_be_dealt
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards_on_hand = []
+
+    def take_card(self, card):
+        self.cards_on_hand.append(card)
+
+    def return_weight_cards(self):
+        weight_card = []
+        for card in self.cards_on_hand:
+            weight_card.append(card.weight_return())
+        if sum(weight_card) > 21:
+            for index, weight in enumerate(weight_card):
+                if weight == 11:
+                    weight_card[index] = 1
+                if sum(weight_card) < 21:
+                    break
+        return sum(weight_card)
+
+    def print_cards(self):
+        for card in self.cards_on_hand:
+            weight, suit = card.return_card()
+            print('{} {}'.format(weight, suit))
+
+
+customization_computer = 18 #до скольки очков компьютер берёт карту
+winner = None
+loser = None
+
+print('Игра "Блек_джек":')
+name = input('Введите имя: ')
+deck = Deck()
+player = Player(name)
+computer = Player('Компьютер')
+
+for _ in range(2):
+    player.take_card(deck.hand_over_card())
+    computer.take_card(deck.hand_over_card())
+player.print_cards()
+
+hand_over_player_flag = True
+hand_over_computer_flag = True
+draw_flag = False
+
+while True:
+    if hand_over_player_flag:
+        question = input('Сдать ещё? ').lower()
+        if question == 'да':
+            player.take_card(deck.hand_over_card())
+            player.print_cards()
+            if player.return_weight_cards() > 21:
+                winner = computer
+                loser = player
+                break
+        else:
+            hand_over_player_flag = False
+
+    if computer.return_weight_cards() <= customization_computer:
+        computer.take_card(deck.hand_over_card())
+        if computer.return_weight_cards() > 21:
+            winner = player
+            loser = computer
+            break
+    else:
+        hand_over_computer_flag = False
+
+    if not hand_over_player_flag and not hand_over_computer_flag:
+        if computer.return_weight_cards() > player.return_weight_cards():
+            winner = computer
+            loser = player
+            break
+        elif player.return_weight_cards() > computer.return_weight_cards():
+            winner = player
+            loser = computer
+            break
+        else:
+            draw_flag = True
+            break
+
+if draw_flag:
+    print('Ничья! ({} : {})'
+          .format(player.name, winner.return_weight_cards(), computer.return_weight_cards()))
+# print('--{}--'.format(computer.name))
+# computer.print_cards()
+# print('------------')
+print('{} победил! ({} : {})'
+      .format(winner.name, winner.return_weight_cards(), loser.return_weight_cards()))
--- Module24/09_tic-tac-toe/main.py
+++ Module24/09_tic-tac-toe/main.py
@@ -1,1 +1,98 @@
-# TODO здесь писать код
+import random
+
+class Cell:
+    def __init__(self, is_busy, number):
+        self.number = number
+        self.is_busy = is_busy
+
+    def return_is_busy(self):
+        return self.is_busy
+
+    def change_state(self, state):
+        if self.return_is_busy() == '-':
+            self.is_busy = state
+
+class Board:
+    def __init__(self):
+        self.board = [Cell('-', number) for number in range(1, 10)]
+
+    def return_cell_is_busy(self, number):
+        return self.board[number].is_busy
+
+    def print_board(self):
+        for i_cell, cell in enumerate(self.board):
+            print(' {}'.format(self.return_cell_is_busy(i_cell)), end='')
+            if (i_cell + 1) % 3 == 0:
+                 print()
+
+    def change_state_cell(self, number, is_busy):
+        self.board[number].change_state(is_busy)
+
+
+class Player:
+    def __init__(self, name, is_computer=False):
+        self.name = name
+        self.is_computer = is_computer
+
+    def make_move(self, board = None):
+        if self.is_computer:
+            if board.return_cell_is_busy(4) == '-':
+                move = 4
+        else:
+            move = int(input()) - 1
+        return move
+
+
+class TicTacToeGame:
+
+    def __init__(self, name1, name2, player2_is_computer):
+        self.board = Board()
+        self.player1 = Player(name1)
+        self.player2 = Player(name2, player2_is_computer)
+        self.players = [self.player1, self.player2]
+        self.cross = set()
+        self.zero = set()
+
+    def who_goes_first(self):
+        first_player = random.randint(0, 1)
+        if first_player == 1:
+            self.players.reverse()
+        print('Первым ходит {}.'.format(self.players[0].name))
+
+    def request_player_move(self, player):
+        if player.is_computer:
+            return player.make_move(self.board)
+        else:
+            print('Ход {}:'.format(player.name))
+            return player.make_move()
+
+    def change_state_cell_board(self, number, is_busy):
+        self.board.change_state_cell(number, is_busy)
+
+    def is_game_over(self):
+        return True
+
+    def game(self):
+        self.who_goes_first()
+        while self.is_game_over():
+            self.change_state_cell_board(self.request_player_move(self.players[0]), 'X')
+            self.board.print_board()
+            self.change_state_cell_board(self.request_player_move(self.players[1]), '0')
+            self.board.print_board()
+
+
+print('Игра "Крестики-нолики"')
+number_players = int(input('Введите количество игроков (1 или 2): '))
+
+if number_players == 1:
+    player1_name = input('Введите имя: ')
+    player2_name = 'Компьютер'
+    player2_is_computer = True
+
+if number_players == 2:
+    player1_name = input('Введите имя первого игрока: ')
+    player2_name = input('Введите имя второго игрока: ')
+    player2_is_computer = False
+
+tic_tac_toe = TicTacToeGame(player1_name, player2_name, player2_is_computer)
+tic_tac_toe.game()
