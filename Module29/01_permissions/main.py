ac66dbaa70de86a698eba7cf9a7676fb228dbfbd
Mihail Druchek <druchek_ma@vsw.ru>
2022-11-21 15:21:35 +0300

    tasks 1,2,3,5 module29 are solved

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def check_permission(user: str) -> Callable:
+    """
+    Дероратор проверяет наличие прав пользователя
+    """
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            try:
+                if user in user_permissions:
+                    result = func(*args, **kwargs)
+                else:
+                    raise PermissionError
+                return result
+            except PermissionError:
+                print('PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию add_comment')
+        return wrapper
+    return decorator
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    """
+    Декоратор для создания синглтона
+    """
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        if wrapper.instance is None:
+            wrapper.instance = cls(*args, **kwargs)
+        return wrapper.instance
+    wrapper.instance = None
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
