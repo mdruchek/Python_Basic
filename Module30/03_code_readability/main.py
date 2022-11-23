280d6ecd54424c68bc9e0b06768890cfb26553ad
Дручек Михаил <druchek_ma@vsw.ru>
2022-11-23 16:58:50 +0300

    module30 is solved

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from typing import List
+
+
+if __name__ == '__main__':
+    prime_numbers_list_compl: List[int] = [num for num in range(2, 1001) if len([divider for divider in range(2, num) if num % divider == 0]) == 0]
+
+    print(prime_numbers_list_compl)
+
+
+    prime_numbers_lambda: List[int] = list(filter(lambda num: num == 2 or len([dev for dev in range(2, num) if num % dev == 0]) == 0, range(2, 1001)))
+
+    print(prime_numbers_lambda)
+
+
+    prime_numbers_list: List[int] = [2]
+    for num in range(2, 1001):
+        prime: bool = True
+        for prime_number in prime_numbers_list:
+            if num % prime_number == 0:
+                prime = False
+        if prime:
+            prime_numbers_list.append(num)
+
+    print(prime_numbers_list)
--- Module30/04_palindrome_the_return/main.py
+++ Module30/04_palindrome_the_return/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+from collections import Counter
+from typing import List
+
+
+if __name__ == '__main__':
+    """
+    Функция для проверки строки на полиндром
+    """
+    def can_be_poly(str_is_poly: str) -> bool:
+        counter: Counter = Counter()
+        for symbol in str_is_poly:
+            counter[symbol] += 1
+        not_even_number: List[str] = list(filter(lambda item: counter[item] % 2 != 0, counter))
+        if len(str_is_poly) % 2 == 0 and len(not_even_number) == 0 or len(str_is_poly) % 2 != 0 and len(not_even_number) == 1:
+            return True
+        return False
+
+
+
+    print(can_be_poly('abcba'))
+    print(can_be_poly('abbbc'))
+    print(can_be_poly('abba'))
+    print(can_be_poly('abca'))
